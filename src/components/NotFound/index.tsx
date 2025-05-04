33071f1b5dc66927fba6313aabb9904b1310e01c
andrei8788 <dalas8788@gmail.com>
2025-05-04 20:51:48 +0300

    Fixed Not found page

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react';
 
+import SadSmileyImg from '@/assets/images/sad-smiley.png';
+
 import style from './notFound.module.scss';
 
 const NotFound: FC = () => {
   return (
     <div className={style.container}>
-      <h1 className={style.title}>404</h1>
+      <div className={style.wrapper}>
+        <img src={SadSmileyImg} alt="Smiley" className={style.smiley} />
+        <h1 className={style.title}>404</h1>
+        <h2 className={style.subtitle}>Page not found</h2>
+      </div>
     </div>
   );
 };
